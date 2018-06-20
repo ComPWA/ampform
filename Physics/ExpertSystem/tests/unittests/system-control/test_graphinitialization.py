0802bc3c484251a5bde1fec6be66718293712ac0
spflueger <spfluege@gmail.com>
2018-06-20 11:11:53 +0200

    test: improve graph initialization test

--- Physics/ExpertSystem/tests/unittests/system-control/test_graphinitialization.py
+++ Physics/ExpertSystem/tests/unittests/system-control/test_graphinitialization.py
@@ -10,11 +10,11 @@ from expertsystem.ui.system_control import (
         ([("Y", [-1])],
          [("D0", [0]), ("D0bar", [0]),
           ("pi0", [0]), ("pi0", [0])],
-         [['D0', 'pi0'], ['D0bar', 'pi0']], 1),
+         [[['D0', 'pi0'], ['D0bar', 'pi0']]], 1),
         ([("Y", [-1, 1])],
          [("D0", [0]), ("D0bar", [0]),
           ("pi0", [0]), ("pi0", [0])],
-         [['D0', 'pi0'], ['D0bar', 'pi0']], 2),
+         [[['D0', 'pi0'], ['D0bar', 'pi0']]], 2),
         ([("Y", [1])],
          [("D0", [0]), ("D0bar", [0]),
           ("pi0", [0]), ("pi0", [0])],
@@ -23,15 +23,19 @@ from expertsystem.ui.system_control import (
          [("D0", [0]), ("D0bar", [0]),
           ("pi0", [0]), ("pi0", [0])],
          [], 18),
+        ([("Y", [1])],
+         [("D0", [0]), ("D0bar", [0]),
+          ("pi0", [0]), ("pi0", [0])],
+         [[['D0', 'pi0'], ['D0bar', 'pi0']], ['D0', 'pi0']], 3),
         ([("J/psi", [-1, 1])],
          [("gamma", [-1, 1]), ("pi0", [0]), ("pi0", [0])],
-         ['pi0', 'pi0'], 4),
+         [['pi0', 'pi0']], 4),
         ([("J/psi", [-1, 1])],
          [("gamma", [-1, 1]), ("pi0", [0]), ("pi0", [0])],
          [], 8),
         ([("J/psi", [-1, 1])],
          [("gamma", [-1, 1]), ("pi0", [0]), ("pi0", [0])],
-         ["pi0", "pi-"], 0)
+         [["pi0", "pi-"]], 0)
     ])
 def test_external_edge_initialization(initial_state, final_state,
                                       final_state_groupings,
@@ -40,7 +44,8 @@ def test_external_edge_initialization(initial_state, final_state,
                                          formalism_type='helicity')
 
     tbd_manager.set_allowed_interaction_types([InteractionTypes.Strong])
-    tbd_manager.add_final_state_grouping(final_state_groupings)
+    for x in final_state_groupings:
+        tbd_manager.add_final_state_grouping(x)
     tbd_manager.number_of_threads = 1
 
     topology_graphs = tbd_manager.build_topologies()
