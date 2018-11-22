5098dc1795e67957a2fa1babc0044b332e70f9ff
Stefan Pflueger <spfluege@gmail.com>
2018-11-22 15:35:32 +0100

    build: add python >3.3 version check

--- Physics/ExpertSystem/expertsystem/__init__.py
+++ Physics/ExpertSystem/expertsystem/__init__.py
@@ -1,1 +1,18 @@
 __all__ = ['amplitude', 'state', 'topology', 'ui']
+
+import sys
+
+
+def print_message_and_exit():
+    print("You are running python "
+          + str(sys.version_info[0]) + "."
+          + str(sys.version_info[1]) + "."
+          + str(sys.version_info[2]))
+    print("The ComPWA expertsystem required python 3.3 or higher!")
+    sys.exit()
+
+
+if sys.version_info.major < 3:
+    print_message_and_exit()
+elif sys.version_info.major == 3 and sys.version_info.minor < 3:
+    print_message_and_exit()
