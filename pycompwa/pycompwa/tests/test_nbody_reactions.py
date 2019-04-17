08b80927d892d1f8e225bdd204dfc29ce8fc8e8f
Stefan Pflueger <spfluege@gmail.com>
2019-04-17 18:17:30 +0200

    feat: add more N-body reaction tests

--- pycompwa/pycompwa/tests/test_nbody_reactions.py
+++ pycompwa/pycompwa/tests/test_nbody_reactions.py
@@ -1,12 +1,11 @@
 from pycompwa.expertsystem.ui.system_control import (
     StateTransitionManager, InteractionTypes)
 
-import logging
+# import logging
+# logging.basicConfig(level=logging.INFO)
 
 import pytest
 
-
-# logging.basicConfig(level=logging.INFO)
 
 @pytest.mark.parametrize("test_input,expected", [
     ((['p', 'pbar'], ['pi+', 'pi0']), ['ChargeConservation']),
@@ -36,6 +35,7 @@ import pytest
     ((['Xi0'], ['p', 'pi-']), []),
     ((['pi-', 'p'], ['lambda', 'K_S0']), []),
     ((['pi0'], ['gamma', 'gamma']), []),
+    ((['pi0'], ['gamma', 'gamma', 'gamma']), []),
     ((['sigma-'], ['n', 'e-', 'vebar']), []),
     ((['rho(770)0'], ['pi0', 'pi0']), ['IdenticalParticleSymmetrization']),
     ((['rho(770)0'], ['gamma', 'gamma']), []),
@@ -66,6 +66,34 @@ def test_general_reaction(test_input, expected):
 
 @pytest.mark.parametrize("test_input,expected", [
     ((['f0(980)'], ['pi+', 'pi-']), []),
+    ((['J/psi'], ['pi0', 'f0(980)']), ['CParityConservation',
+                                       'SpinConservation']),
+    ((['pi0'], ['gamma', 'gamma']), []),
+    ((['pi0'], ['gamma', 'gamma', 'gamma']), [
+     'CParityConservation', 'SpinConservation']),
+    ((['pi0'], ['e+', 'e-', 'gamma']), []),
+    ((['pi0'], ['e+', 'e-']), [])
+])
+def test_em_reactions(test_input, expected):
+    # general checks
+    print("processing case:" + str(test_input))
+
+    tbd_manager = StateTransitionManager(test_input[0], test_input[1],
+                                         formalism_type='canonical',
+                                         topology_building='nbody',
+                                         propagation_mode='full')
+
+    tbd_manager.set_allowed_interaction_types([InteractionTypes.EM])
+
+    graph_interaction_settings = tbd_manager.prepare_graphs()
+    (solutions, violated_rules) = tbd_manager.find_solutions(
+        graph_interaction_settings)
+
+    assert set(violated_rules) == set(expected)
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ((['f0(980)'], ['pi+', 'pi-']), []),
     ((['J/psi'], ['pi0', 'f0(980)']), ['IsoSpinConservation',
                                        'CParityConservation',
                                        'SpinConservation'])
@@ -86,9 +114,3 @@ def test_strong_reactions(test_input, expected):
         graph_interaction_settings)
 
     assert set(violated_rules) == set(expected)
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    test_general_reaction()
-    test_strong_reactions()
