9320068cd28ad66caa1a3b630fda2500c9ed4a2e
Remco de Boer <remco@ihep.ac.cn>
2020-05-07 12:04:08 +0200

    docs: add description to main module (ComPWA/expertsystem#20)
    * chore: remove graphvis
    * docs: improve description expertsystem module
    * docs: fix link to contribute page

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,4 +1,4 @@
 # How to contribute?
 
 See instructions at
-[tensorwaves.readthedocs.io/contribute](https://pwa.readthedocs.io/projects/expertsystem/contribute)!
+[tensorwaves.readthedocs.io/contribute](https://pwa.readthedocs.io/projects/expertsystem/en/latest/contribute.html)!
--- expertsystem/__init__.py
+++ expertsystem/__init__.py
@@ -1,13 +1,38 @@
-__all__ = ["amplitude", "state", "topology", "ui"]
+"""
+The expert system facilitates building an amplitude model.
+
+An amplitude model describes the reaction process you want to study with
+partial wave analysis techniques. The responsibility of the expert system is to
+give advice on the form of an amplitude model based on the problem set one
+defines for a reaction process (initial state, final state, allowed
+interactions, intermediate states, etc.). Internally, the system propagates the
+quantum numbers through the reaction graph, while satisfying the specified
+conservation rules.
+
+Afterwards, the amplitude model of the expert system can be exported. This
+amplitude model can then for instance be used to generate a data set (toy Monte
+Carlo) for this specific reaction process, or to optimize ('fit') its
+parameters so that they resemble the data set as good as possible.
+"""
+
+
+__all__ = [
+    "amplitude",
+    "state",
+    "topology",
+    "ui",
+]
+
+
+import sys
+
 from . import amplitude
 from . import state
 from . import topology
 from . import ui
 
 
-if __name__ == "__main__":
-    import sys
-
+def __check_python_version():
     def print_message_and_exit():
         print(
             "You are running python "
@@ -17,10 +42,14 @@ if __name__ == "__main__":
             + "."
             + str(sys.version_info[2])
         )
-        print("The ComPWA expertsystem required python 3.3 or higher!")
+        print("The expertsystem module requires Python 3.3 or higher!")
         sys.exit()
 
     if sys.version_info.major < 3:
         print_message_and_exit()
     elif sys.version_info.major == 3 and sys.version_info.minor < 3:
         print_message_and_exit()
+
+
+if __name__ == "__main__":
+    __check_python_version()
