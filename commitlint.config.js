271db280f0c258db3792ed3da17d5d21d5178c1c
Remco de Boer <remco@ihep.ac.cn>
2020-08-03 15:26:05 +0200

    ci: fix missing commitlint config (ComPWA/expertsystem#148)

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "ci",
+        "chore",
+        "docs",
+        "feat",
+        "fix",
+        "refactor",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
