73d6049bac87c9646b87deb2e3bc398c3b3d0978
Remco de Boer <remco@ihep.ac.cn>
2020-08-26 09:48:44 +0200

    test: reorganize J/psi to gamma pi0 pi0 (ComPWA/expertsystem#233)

--- tests/channels/test_jpsi_to_gamma_pi0_pi0.py
+++ tests/channels/test_jpsi_to_gamma_pi0_pi0.py
@@ -17,30 +17,54 @@ logging.basicConfig(level=logging.ERROR)
 logging.getLogger().setLevel(logging.ERROR)
 
 
+@pytest.mark.parametrize(
+    "allowed_intermediate_particles, number_of_solutions",
+    [
+        (["f(0)(1500)"], 4),
+        (["f(0)(980)", "f(0)(1500)"], 8),
+        (["f(2)(1270)"], 12),
+        (["omega(782)"], 16),
+        (
+            [
+                "f(0)(980)",
+                "f(0)(1500)",
+                "f(2)(1270)",
+                "f(2)(1950)",
+                "omega(782)",
+            ],
+            48,
+        ),
+    ],
+)
 @pytest.mark.slow
-def test_script():
+def test_number_of_solutions(
+    allowed_intermediate_particles, number_of_solutions
+):
     stm = StateTransitionManager(
         initial_state=[("J/psi(1S)", [-1, 1])],
         final_state=[("gamma", [-1, 1]), ("pi0", [0]), ("pi0", [0])],
-        allowed_intermediate_particles=[
-            "f(0)(980)",
-            "f(0)(1500)",
-            "f(2)(1270)",
-            "f(2)(1950)",
-            "omega(782)",
-        ],
+        allowed_intermediate_particles=allowed_intermediate_particles,
+        number_of_threads=1,
     )
-    stm.number_of_threads = 1
     stm.set_allowed_interaction_types(
         [InteractionTypes.Strong, InteractionTypes.EM]
     )
-
     graph_interaction_settings_groups = stm.prepare_graphs()
     solutions, _ = stm.find_solutions(graph_interaction_settings_groups)
+    assert len(solutions) == number_of_solutions
 
-    print("found " + str(len(solutions)) + " solutions!")
-    assert len(solutions) == 48
 
+def test_id_to_particle_mappings():
+    stm = StateTransitionManager(
+        initial_state=[("J/psi(1S)", [-1, 1])],
+        final_state=[("gamma", [-1, 1]), ("pi0", [0]), ("pi0", [0])],
+        allowed_intermediate_particles=["f(0)(980)"],
+        number_of_threads=1,
+    )
+    stm.set_allowed_interaction_types([InteractionTypes.Strong])
+    graph_interaction_settings_groups = stm.prepare_graphs()
+    solutions, _ = stm.find_solutions(graph_interaction_settings_groups)
+    assert len(solutions) == 4
     ref_mapping_fs = _create_edge_id_particle_mapping(
         solutions[0], "get_final_state_edges"
     )
@@ -54,16 +78,3 @@ def test_script():
         assert ref_mapping_is == _create_edge_id_particle_mapping(
             solution, "get_initial_state_edges"
         )
-
-    print("intermediate states:")
-    intermediate_states = set()
-    for solution in solutions:
-        int_edge_id = solution.get_intermediate_state_edges()[0]
-        intermediate_states.add(solution.edge_props[int_edge_id]["Name"])
-    print(intermediate_states)
-
-    stm.write_amplitude_model(solutions, "JPsiToGammaPi0Pi0.xml")
-
-
-if __name__ == "__main__":
-    test_script()
