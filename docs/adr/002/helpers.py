307d693fd117bb8638d1d17a779a9829f4b5c38f
Remco de Boer <remco@ihep.ac.cn>
2021-03-11 14:53:02 +0100

    style: abbreviate sympy to sp, not sy (ComPWA/expertsystem#509)

--- docs/adr/002/helpers.py
+++ docs/adr/002/helpers.py
@@ -2,51 +2,51 @@
 
 from typing import List
 
-import sympy as sy  # pyright: reportMissingImports=false
+import sympy as sp  # pyright: reportMissingImports=false
 
 
 class StateTransitionGraph:
     pass
 
 
-def hankel1(angular_momentum: sy.Symbol, x: sy.Symbol) -> sy.Expr:
+def hankel1(angular_momentum: sp.Symbol, x: sp.Symbol) -> sp.Expr:
     x_squared = x ** 2
-    return sy.Piecewise(
+    return sp.Piecewise(
         (
             1,
-            sy.Eq(angular_momentum, 0),
+            sp.Eq(angular_momentum, 0),
         ),
         (
             1 + x_squared,
-            sy.Eq(angular_momentum, 1),
+            sp.Eq(angular_momentum, 1),
         ),
         (
             9 + x_squared * (3 + x_squared),
-            sy.Eq(angular_momentum, 2),
+            sp.Eq(angular_momentum, 2),
         ),
         (
             225 + x_squared * (45 + x_squared * (6 + x_squared)),
-            sy.Eq(angular_momentum, 3),
+            sp.Eq(angular_momentum, 3),
         ),
         (
             1575 + x_squared * (135 + x_squared * (10 + x_squared)),
-            sy.Eq(angular_momentum, 4),
+            sp.Eq(angular_momentum, 4),
         ),
     )
 
 
 def blatt_weisskopf(
-    q: sy.Symbol, q_r: sy.Symbol, angular_momentum: sy.Symbol
-) -> sy.Expr:
-    return sy.sqrt(
+    q: sp.Symbol, q_r: sp.Symbol, angular_momentum: sp.Symbol
+) -> sp.Expr:
+    return sp.sqrt(
         abs(hankel1(angular_momentum, q)) ** 2
         / abs(hankel1(angular_momentum, q_r)) ** 2
     )
 
 
 def two_body_momentum_squared(
-    m_d: sy.Symbol, m_a: sy.Symbol, m_b: sy.Symbol
-) -> sy.Expr:
+    m_d: sp.Symbol, m_a: sp.Symbol, m_b: sp.Symbol
+) -> sp.Expr:
     return (
         (m_d ** 2 - (m_a + m_b) ** 2)
         * (m_d ** 2 - (m_a - m_b) ** 2)
