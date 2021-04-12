39fd8e8c40f101f03a9536d8ee0a24ced34417a7
Remco de Boer <redeboer@gmx.com>
2021-04-12 14:32:44 +0200

    style: rename sympy_model to model

--- tests/test_helicity.py
+++ tests/test_helicity.py
@@ -19,11 +19,11 @@ def test_generate(
         result = jpsi_to_gamma_pi_pi_helicity_solutions
     else:
         raise NotImplementedError
-    sympy_model = get_builder(result).generate()
-    assert len(sympy_model.parameter_defaults) == 2
-    assert len(sympy_model.components) == 4 + n_amplitudes
+    model = get_builder(result).generate()
+    assert len(model.parameter_defaults) == 2
+    assert len(model.components) == 4 + n_amplitudes
 
-    free_symbol_names = {s.name for s in sympy_model.expression.free_symbols}
+    free_symbol_names = {s.name for s in model.expression.free_symbols}
     assert free_symbol_names == {
         R"C[J/\psi(1S) \to f_{0}(1500)_{0} \gamma_{+1};f_{0}(1500) \to \pi^{0}_{0} \pi^{0}_{0}]",
         R"C[J/\psi(1S) \to f_{0}(980)_{0} \gamma_{+1};f_{0}(980) \to \pi^{0}_{0} \pi^{0}_{0}]",
