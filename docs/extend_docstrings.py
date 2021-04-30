f09594dabc8cb9547dbb7e49066ce282503e93d6
Remco de Boer <redeboer@gmx.com>
2021-04-30 11:42:06 +0200

    docs: simplify latex of rel BW with ff using q(m)

--- docs/extend_docstrings.py
+++ docs/extend_docstrings.py
@@ -44,21 +44,23 @@ update_docstring(
     breakup_momentum,
     f"""
 .. math:: q^2(m) = {sp.latex(q ** 2)}
+    :label: breakup_momentum
 """,
 )
 
 
-m, m0, w0 = sp.symbols("m m0 Gamma", real=True)
+m, m0, w0 = sp.symbols("m m0 Gamma")
 rel_bw = relativistic_breit_wigner(m, m0, w0)
 update_docstring(
     relativistic_breit_wigner,
     f"""
 .. math:: {sp.latex(rel_bw)}
+    :label: relativistic_breit_wigner
 """,
 )
 
 
-m, m0, w0, ma, mb, d = sp.symbols("m m0 Gamma m_a m_b d", real=True)
+m, m0, w0, ma, mb, d = sp.symbols("m m0 Gamma m_a m_b d")
 rel_bw_with_ff = relativistic_breit_wigner_with_ff(
     mass=m,
     mass0=m0,
@@ -68,11 +70,23 @@ rel_bw_with_ff = relativistic_breit_wigner_with_ff(
     angular_momentum=0,
     meson_radius=d,
 )
+q = breakup_momentum(m, m_a, m_b)
+q0 = breakup_momentum(m0, m_a, m_b)
+rel_bw_with_ff = rel_bw_with_ff.subs(
+    {
+        2 * q: sp.Symbol("q^{2}(m)"),
+        2 * q0: sp.Symbol("q^{2}(m_0)"),
+    }
+)
 update_docstring(
     relativistic_breit_wigner_with_ff,
     f"""
-For :math:`L=0`, this lineshape has the following form:
+For :math:`L=0`, a relativistic Breit-Wigner with `.BlattWeisskopf` form factor
+has the following form:
 
 .. math:: {sp.latex(rel_bw_with_ff.doit())}
+    :label: relativistic_breit_wigner_with_ff
+
+where :math:`q^2(m)` is defined by :eq:`breakup_momentum`.
 """,
 )
