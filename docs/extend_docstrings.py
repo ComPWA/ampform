20be35a80cce644e0cb6ea8e364f612060b506e5
Remco de Boer <redeboer@gmx.com>
2021-06-10 18:15:09 +0200

    fix: rho-hat description in phase_space_factor_ac

--- docs/extend_docstrings.py
+++ docs/extend_docstrings.py
@@ -14,6 +14,7 @@ import sympy as sp
 from ampform.dynamics import (
     BlattWeisskopfSquared,
     _analytic_continuation,
+    _phase_space_factor_hat,
     breakup_momentum_squared,
     coupled_width,
     phase_space_factor,
@@ -129,14 +130,18 @@ def render_phase_space_factor() -> None:
 def render_phase_space_factor_ac() -> None:
     s, m_a, m_b, rho = sp.symbols(R"s, m_a, m_b, \hat{\rho}")
     rho_analytic = _analytic_continuation(rho, s, s_threshold=(m_a + m_b) ** 2)
+    rho_hat = _phase_space_factor_hat(s, m_a, m_b)
     update_docstring(
         phase_space_factor_ac,
         fR"""
     .. math:: {sp.latex(rho_analytic)}
         :label: phase_space_factor_ac
 
-    with :math:`\hat{{\rho}}` the :func:`phase_space_factor` defined by
-    :eq:`phase_space_factor`.
+    with :math:`\hat{{\rho}}` a slightly adapted :func:`phase_space_factor`
+    that takes the absolute value of :func:`.breakup_momentum_squared`:
+
+    .. math:: {sp.latex(rho_hat)}
+        :label: rho_hat
     """,
     )
 
