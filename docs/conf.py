b6ff112eb6d92f1904fc8effe3118346c35f5e40
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2021-12-13 11:10:44 +0100

    fix: remap linked versions in documentation
    Some pinned versions have no API website (yet). This allows to remap to
    existing version pages.

--- docs/conf.py
+++ docs/conf.py
@@ -207,6 +207,11 @@ nitpick_ignore = [
 
 
 # Intersphinx settings
+version_remapping = {
+    "matplotlib": {"3.5.1": "3.5.0"},
+}
+
+
 def get_version(package_name: str) -> str:
     python_version = f"{sys.version_info.major}.{sys.version_info.minor}"
     constraints_path = f"../.constraints/py{python_version}.txt"
@@ -224,6 +229,11 @@ def get_version(package_name: str) -> str:
             continue
         _, installed_version, *_ = line_segments
         installed_version = installed_version.strip()
+        remapped_versions = version_remapping.get(package_name)
+        if remapped_versions is not None:
+            existing_version = remapped_versions.get(installed_version)
+            if existing_version is not None:
+                return existing_version
         return installed_version
     return "stable"
 
