3332c17e43e34fa1ef7f9b34f098ff23c72a3286
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-01-31 11:10:45 +0100

    docs: explain ArrayMultiplication class

--- src/ampform/sympy/_array_expressions.py
+++ src/ampform/sympy/_array_expressions.py
@@ -355,6 +355,16 @@ class ArrayAxisSum(sp.Expr):
 
 
 class ArrayMultiplication(sp.Expr):
+    r"""Contract rank-:math:`n` arrays and a rank-:math`n-1` array.
+
+    This class is particularly useful to create a tensor product of rank-3
+    matrix array classes, such as `.BoostZ`, `.RotationY`, and `.RotationZ`,
+    with a rank-2 `.FourMomentumSymbol`. In that case, if :math:`n` is the
+    number of events, you would get a contraction of arrays of shape
+    :math:`n\times\times4\times4` (:math:`n` Lorentz matrices) to
+    :math:`n\times\times4` (:math:`n` four-momentum tuples).
+    """
+
     def __new__(cls, *tensors: sp.Expr, **hints: Any) -> "ArrayMultiplication":
         return create_expression(cls, *tensors, **hints)
 
