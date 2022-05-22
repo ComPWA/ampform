6c013a73ae0474a87a64e903c0f2db383d098ee6
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-05-22 16:19:56 +0200

    test: add test function for is_within_phasespace
    This function uses all kinematics.phasespace functions and suffices for
    now.

---
+++ tests/kinematics/test_phasespace.py
@@ -0,0 +1,21 @@
+import pytest
+
+from ampform.kinematics.phasespace import is_within_phasespace
+
+
+@pytest.mark.parametrize(
+    ("s1", "s2", "expected"),
+    [
+        (0.0, 3.0, 0),
+        (1.0, 1.0, 1),
+        (2.0, 2.0, 1),
+    ],
+)
+def test_is_within_phasespace(s1, s2, expected):
+    # See widget https://compwa-org.rtfd.io/report/017.html
+    m0 = 2.1
+    m1 = 0.2
+    m2 = 0.4
+    m3 = 0.4
+    computed = is_within_phasespace(s1, s2, m0, m1, m2, m3, outside_value=0)
+    assert computed.doit() == expected
