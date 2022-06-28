8bbd262567518885aa0b29b965aa3d4a2987ffe9
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-06-28 14:38:04 +0200

    ci: add linkcheck ignore

--- docs/conf.py
+++ docs/conf.py
@@ -331,6 +331,7 @@ copybutton_prompt_text = r">>> |\.\.\. "  # doctest
 # Settings for linkcheck
 linkcheck_anchors = False
 linkcheck_ignore = [
+    "http://www.curtismeyer.com",
     "https://doi.org/10.1093/ptep/ptaa104",
     "https://home.fnal.gov/~kutschke/Angdist/angdist.ps",
     "https://hss-opus.ub.ruhr-uni-bochum.de",
