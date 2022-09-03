36c335e070262d2244232284d8067a7c23e43bc6
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-09-03 23:44:40 +0200

    MAINT: update pip constraints and pre-commit config (#324)
    * FIX: update links to SymPy tutorial pages
    * MAINT: ignore `mystnb.unknown_mime_type` (`application/json`)
      https://github.com/ComPWA/ampform/runs/8132373732?check_suite_focus=true#step:5:127
    * MAINT: set back `ipynb` indent size to 1
    * MAINT: update allowed labels and commit types
    
    Co-authored-by: GitHub <noreply@github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_kinematics.py
+++ tests/test_kinematics.py
@@ -674,28 +674,34 @@ def test_compute_boost_chain(
         ),
         (
             1,
-            "MatrixMultiplication(BoostMatrix(NegativeMomentum(p1)),"
-            " BoostMatrix(p1 + p2 + p3),"
-            " BoostMatrix(ArrayMultiplication(BoostMatrix(p1 + p2 + p3),"
-            " p1)))",
+            (
+                "MatrixMultiplication(BoostMatrix(NegativeMomentum(p1)),"
+                " BoostMatrix(p1 + p2 + p3),"
+                " BoostMatrix(ArrayMultiplication(BoostMatrix(p1 + p2 + p3),"
+                " p1)))"
+            ),
         ),
         (
             2,
-            "MatrixMultiplication(BoostMatrix(NegativeMomentum(p2)), BoostMatrix(p1 +"
-            " p2 + p3), BoostMatrix(ArrayMultiplication(BoostMatrix(p1 + p2 + p3), p2 +"
-            " p3)),"
-            " BoostMatrix(ArrayMultiplication(BoostMatrix(ArrayMultiplication(BoostMatrix(p1"
-            " + p2 + p3), p2 + p3)), ArrayMultiplication(BoostMatrix(p1 + p2 + p3),"
-            " p2))))",
+            (
+                "MatrixMultiplication(BoostMatrix(NegativeMomentum(p2)), BoostMatrix(p1"
+                " + p2 + p3), BoostMatrix(ArrayMultiplication(BoostMatrix(p1 + p2 +"
+                " p3), p2 + p3)),"
+                " BoostMatrix(ArrayMultiplication(BoostMatrix(ArrayMultiplication(BoostMatrix(p1"
+                " + p2 + p3), p2 + p3)), ArrayMultiplication(BoostMatrix(p1 + p2 + p3),"
+                " p2))))"
+            ),
         ),
         (
             3,
-            "MatrixMultiplication(BoostMatrix(NegativeMomentum(p3)), BoostMatrix(p1 +"
-            " p2 + p3), BoostMatrix(ArrayMultiplication(BoostMatrix(p1 + p2 + p3), p2 +"
-            " p3)),"
-            " BoostMatrix(ArrayMultiplication(BoostMatrix(ArrayMultiplication(BoostMatrix(p1"
-            " + p2 + p3), p2 + p3)), ArrayMultiplication(BoostMatrix(p1 + p2 + p3),"
-            " p3))))",
+            (
+                "MatrixMultiplication(BoostMatrix(NegativeMomentum(p3)), BoostMatrix(p1"
+                " + p2 + p3), BoostMatrix(ArrayMultiplication(BoostMatrix(p1 + p2 +"
+                " p3), p2 + p3)),"
+                " BoostMatrix(ArrayMultiplication(BoostMatrix(ArrayMultiplication(BoostMatrix(p1"
+                " + p2 + p3), p2 + p3)), ArrayMultiplication(BoostMatrix(p1 + p2 + p3),"
+                " p3))))"
+            ),
         ),
     ],
 )
