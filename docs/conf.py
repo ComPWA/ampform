9ccaaf57b9e56a85960e3ced273c340f9aef458b
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-11-25 14:44:53 +0100

    DX: implement GitHub Actions caching (#335)
    * DX: activate and configure VSCode flake8 extension
    * DX: activate and configure VSCode isort extension
    * DX: activate and configure VSCode pylint extension
    * DX: extend pyright file exclusions
    * DX: implement GitHub Actions caching
    * DX: run upgrade cron job on 7th day of the month
    * FIX: ignore `physique.cuso.ch`
      https://github.com/ComPWA/ampform/actions/runs/3547387830/jobs/5957455069#step:6:312
    * MAINT: address mypy issues
    * MAINT: address pyright issues
    * MAINT: remove deprecated `set-output`
      https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands
    * MAINT: split out `jupyter` section for extras_require
    * MAINT: update cSpell config
    * MAINT: update pip constraints and pre-commit
    
    Co-authored-by: GitHub <noreply@github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,6 @@ import sys
 
 import requests
 
-# pyright: reportConstantRedefinition=false
 # pyright: reportMissingImports=false
 # pyright: reportUntypedBaseClass=false
 # pyright: reportUntypedFunctionDecorator=false
@@ -47,11 +46,16 @@ copyright = "2020, ComPWA"  # noqa: A001
 author = "Common Partial Wave Analysis"
 
 # https://docs.readthedocs.io/en/stable/builds.html
-BRANCH = os.environ.get("READTHEDOCS_VERSION", "stable")
-if BRANCH == "latest":
-    BRANCH = "main"
-if re.match(r"^\d+$", BRANCH):  # PR preview
-    BRANCH = "stable"
+def get_branch_name() -> str:
+    branch_name = os.environ.get("READTHEDOCS_VERSION", "stable")
+    if branch_name == "latest":
+        return "main"
+    if re.match(r"^\d+$", branch_name):  # PR preview
+        return "stable"
+    return branch_name
+
+
+BRANCH = get_branch_name()
 
 try:
     __VERSION = get_package_version(PACKAGE)
