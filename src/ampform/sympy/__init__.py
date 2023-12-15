331cbfb8aa15419bfe1d68e517ff3dce84f5e800
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-12-15 12:40:11 +0100

    FEAT: implement `unevaluated_expression` decorator (#365)
    * DOC: add notebook for SymPy helper functions
    * DOC: illustrate usage of `PoolSum`
    * DX: skip ipywidgets tests that copy widgets
    * DX: upgrade developer environment to Python 3.11
    * ENH: make implementation method public as `evaluate()`
    * FEAT: implement `unevaluated_expression` decorator
    * MAINT: update Codecov config style

--- src/ampform/sympy/__init__.py
+++ src/ampform/sympy/__init__.py
@@ -1,4 +1,12 @@
-"""Tools that facilitate in building :mod:`sympy` expressions."""
+"""Tools that facilitate in building :mod:`sympy` expressions.
+
+.. autodecorator:: unevaluated_expression
+.. dropdown:: SymPy assumptions
+
+    .. autodata:: ExprClass
+    .. autoclass:: SymPyAssumptions
+
+"""
 
 # cspell:ignore mhash
 from __future__ import annotations
@@ -18,6 +26,12 @@ from typing import TYPE_CHECKING, Callable, Iterable, Sequence, SupportsFloat, T
 import sympy as sp
 from sympy.printing.conventions import split_super_sub
 from sympy.printing.precedence import PRECEDENCE
+
+from ._decorator import (
+    ExprClass,  # noqa: F401  # pyright: ignore[reportUnusedImport]
+    SymPyAssumptions,  # noqa: F401  # pyright: ignore[reportUnusedImport]
+    unevaluated_expression,  # noqa: F401  # pyright: ignore[reportUnusedImport]
+)
 
 if TYPE_CHECKING:
     from sympy.printing.latex import LatexPrinter
