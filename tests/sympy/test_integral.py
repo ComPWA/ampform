e3c372ab0ad490bb15da11e7ef64e11ef835fa43
Vitor Shen <17490173+shenvitor@users.noreply.github.com>
2024-01-31 12:11:56 +0100

    write simple test real value integral

--- tests/sympy/test_integral.py
+++ tests/sympy/test_integral.py
@@ -1,2 +1,11 @@
-def test_my_function():
-    assert 1 + 1 == 2
+import sympy as sp
+
+from ampform.sympy import UnevaluatableIntegral
+
+
+class TestUnevaluatableIntegral:
+    def test_real_value_function(self):
+        x = sp.symbols("x")
+        integral_expr = UnevaluatableIntegral(x**2, (x, 1, 3))
+        func = sp.lambdify(args=[], expr=integral_expr)
+        assert func() == 26 / 3
