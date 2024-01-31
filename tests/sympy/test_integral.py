ae20f10ad8281cc94d4324719eae35e2d4509d8d
Vitor Shen <17490173+shenvitor@users.noreply.github.com>
2024-01-31 13:42:36 +0100

    write simple test array value parameter function

--- tests/sympy/test_integral.py
+++ tests/sympy/test_integral.py
@@ -1,3 +1,4 @@
+import pytest
 import sympy as sp
 
 from ampform.sympy import UnevaluatableIntegral
@@ -9,3 +10,10 @@ class TestUnevaluatableIntegral:
         integral_expr = UnevaluatableIntegral(x**2, (x, 1, 3))
         func = sp.lambdify(args=[], expr=integral_expr)
         assert func() == 26 / 3
+
+    def test_array_value_parameter_function(self):
+        x, p = sp.symbols("x,p")
+        integral_expr = UnevaluatableIntegral(x**p, (x, 1, 3))
+        func = sp.lambdify(args=[p], expr=integral_expr)
+        assert func(p=2) == 26 / 3
+        assert pytest.approx(func(p=1)) == 4
