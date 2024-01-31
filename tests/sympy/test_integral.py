69b926ae6913102dcef37e10d00533b823170b44
Vitor Shen <17490173+shenvitor@users.noreply.github.com>
2024-01-31 14:38:53 +0100

    add complex function and array input

--- tests/sympy/test_integral.py
+++ tests/sympy/test_integral.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 import sympy as sp
 
@@ -16,9 +17,14 @@ class TestUnevaluatableIntegral:
         [
             (2, 26 / 3),
             (1, 4),
+            (1j, (1 / 2 - 1j / 2) * (-1 + 3 ** (1 + 1j))),
+            (
+                np.array([0, 0.5, 1, 2]),
+                np.array([2, 2 * 3 ** (1 / 2) - 2 / 3, 4, 8 + 2 / 3]),
+            ),
         ],
     )
-    def test_array_value_parameter_function(self, p_value, expected):
+    def test_vectorized_parameter_function(self, p_value, expected):
         x, p = sp.symbols("x,p")
         integral_expr = UnevaluatableIntegral(x**p, (x, 1, 3))
         func = sp.lambdify(args=[p], expr=integral_expr)
