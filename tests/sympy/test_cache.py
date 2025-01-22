9b84bf5f7578c3e052c6b76378c5864717f84df0
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2025-01-22 13:24:43 +0100

    FIX: update expected hashses for different Python versions

--- tests/sympy/test_cache.py
+++ tests/sympy/test_cache.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import sys
 from typing import TYPE_CHECKING, ClassVar
 
 import pytest
@@ -62,8 +63,14 @@ class TestLargeHash:
     @pytest.mark.parametrize(
         ("expected_hash", "formalism"),
         [
-            ("762cc00", "canonical-helicity"),
-            ("17fefe5", "helicity"),
+            (
+                "762cc00" if sys.version_info >= (3, 11) else "1f5ac33",
+                "canonical-helicity",
+            ),
+            (
+                "17fefe5" if sys.version_info >= (3, 11) else "7b5fad1",
+                "helicity",
+            ),
         ],
         ids=["canonical-helicity", "helicity"],
     )
