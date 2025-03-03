912c7dcd61de205550083c0c04d941d00cc06127
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2025-03-03 11:13:09 +0100

    ENH: move `perform_cached_` functions to `cached` module

---
+++ src/ampform/sympy/cached.py
@@ -0,0 +1,35 @@
+"""Handy aliases for working with cached SymPy expressions."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from ampform.sympy._cache import cache_to_disk
+
+if TYPE_CHECKING:
+    from collections.abc import Mapping
+
+    import sympy as sp
+
+
+@cache_to_disk
+def doit(unevaluated_expr: sp.Expr) -> sp.Expr:
+    """Perform :meth:`~sympy.core.basic.Basic.doit` and cache the result to disk.
+
+    The cached result is fetched from disk if the hash of the original expression is the
+    same as the hash embedded in the filename (see :func:`.get_readable_hash`).
+
+    Args:
+        unevaluated_expr: A `sympy.Expr <sympy.core.expr.Expr>` on which to call
+            :meth:`~sympy.core.basic.Basic.doit`.
+
+    .. versionadded:: 0.14.4
+    .. automodule:: ampform.sympy._cache
+    """
+    return unevaluated_expr.doit()
+
+
+@cache_to_disk
+def xreplace(expr: sp.Expr, substitutions: Mapping[sp.Basic, sp.Basic]) -> sp.Expr:
+    """Call :meth:`~sympy.core.basic.Basic.xreplace` and cache the result to disk."""
+    return expr.xreplace(substitutions)
