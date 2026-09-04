40d66d60e6a20434e98f2fd513578662a6084fcd
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2026-09-04 15:09:26 +0200

    FIX: restore `html_static_path` in Sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -214,6 +214,7 @@ html_show_copyright = False
 html_show_sourcelink = False
 html_show_sphinx = False
 html_sourcelink_suffix = ""
+html_static_path = ["_static"]
 html_theme = "sphinx_book_theme"
 html_theme_options = {
     "icon_links": [
